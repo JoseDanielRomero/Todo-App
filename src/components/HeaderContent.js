965125ec9d72441301b54ab8da411fd07f78c928
Jose Daniel Romero <danyrome12@gmail.com>
2023-06-16 17:05:39 -0500

    Se estructuran los 2 principales componentes de la pagina, incluyendo el logo y el toogle button

---
+++ src/components/HeaderContent.js
@@ -0,0 +1,11 @@
+import '../stylesheets/HeaderContent.css'
+
+function HeaderContent() {
+    return (
+        <header className='header-container'>
+            <div className='header-image-light'></div>
+        </header>
+    )
+}
+
+export default HeaderContent;
---
+++ src/components/MainContent.js
@@ -0,0 +1,17 @@
+import '../stylesheets/MainContent.css'
+
+function MainContent() {
+    return (
+        <main className='main-container'>
+            <article className='logo-and-toogle-container'>
+                <h1 className='logo'>
+                    TODO
+                </h1>
+                <button className='toogle-button' />
+            </article>
+            
+        </main>
+    )
+}
+
+export default MainContent;
